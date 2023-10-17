e7dab117093b66da8e0a7ec6a1c8166f688cdb56
Biju Kunjummen <biju.kunjummen@gmail.com>
2023-10-16 21:00:03 -0700

    With basic chat functionality

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
 import streamlit as st
+from dataclasses import dataclass
+
+
+@dataclass
+class Message:
+    actor: str
+    payload: str
+
+
+USER = "user"
+ASSISTANT = "ai"
+MESSAGES = "messages"
+if MESSAGES not in st.session_state:
+    st.session_state[MESSAGES] = [Message(actor=ASSISTANT, payload="Hi!How can I help you?")]
+
+msg: Message
+for msg in st.session_state[MESSAGES]:
+    st.chat_message(msg.actor).write(msg.payload)
 
 prompt: str = st.chat_input("Enter a prompt here")
 
-USER = "user"
-ASSISTANT = "assistant"
-
 if prompt:
+    st.session_state[MESSAGES].append(Message(actor=USER, payload=prompt))
     st.chat_message(USER).write(prompt)
-    st.chat_message(ASSISTANT).write(f"You wrote {prompt}")
+    response: str = f"You wrote {prompt}"
+    st.session_state[MESSAGES].append(Message(actor=ASSISTANT, payload=response))
+    st.chat_message(ASSISTANT).write(response)
